52eb5adbdb5dddb9c23f3b874e333fd83f31de4a
unknown <nick_clymer2018@yahoo.com>
2021-03-13 16:45:17 -0700

    updated html. added time subtraction function to timer, setup question array to display as an indexed series, added score keeper.

--- quiz-script.js
+++ quiz-script.js
@@ -1,102 +1,124 @@
+window.onload = function () {
 
-window.onload = function() {
+    var timeEl = document.querySelector(".timer");
+    var mainEl = document.getElementById("quiz-text");
+    var startButton = document.querySelector(".start-btn");
+    var currentIndx = 0;
+    var score = 0;
+    var secondsLeft = 60;
 
-//quiz timer
-var timeEl = document.querySelector(".timer");
-var mainEl = document.getElementById("quiz-text");
-var startButton = document.querySelector(".start-btn");
-
-function setTime() {
-    startButton.disabled = true;
-    var secondsLeft = 5;
-    var timerInterval = setInterval(function() {
-    secondsLeft--;    
-    timeEl.textContent = "Seconds left until quiz ends! " + secondsLeft + ".";
-
-    if(secondsLeft <= 0) {
-      clearInterval(timerInterval);
-      startButton.disabled = false;
-      // Calls function to create and append image
-      sendMessage(); //create function call for final score and initals.
+    function cleanPage() {
+        if (startButton.disabled = false);
+            document.getElementsById(askQuestion).innerHTML = "";
+        cleanPage();
+        console.log(cleanPage);
     }
-  }, 1000);
-}
 
-//quiz question array
-var questions = [
-    {
-        question: "Commonly used data types DO NOT include:?",
-        options: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts",
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within ____?",
-        options: ["quotes","curly brackets", "parentheses", "square brackets"],
-        answer: "curley brackets",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ____.",
-        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above",
-    },
-    {
-        question: "String values must be enclosed within _____ when being assinged to variables.",
-        options: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes",
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:____?",
-        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        answer: "console.log",
+    //quiz timer
+    function setTime() {
+        startButton.disabled = true;
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timeEl.textContent = "Seconds left until quiz ends! " + secondsLeft + ".";
+
+            if (secondsLeft <= 0) {
+                clearInterval(timerInterval);
+                startButton.disabled = false;
+                addInitals();
+            }
+        }, 1000);
     }
-];
 
-const answersUl = document.getElementById('answers');
+    //quiz question array
+    var questions = [
+        {
+            question: "Commonly used data types DO NOT include:?",
+            options: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts",
+        },
+        {
+            question: "The condition in an if/else statement is enclosed within ____?",
+            options: ["quotes", "curley brackets", "parentheses", "square brackets"],
+            answer: "parentheses",
+        },
+        {
+            question: "Arrays in JavaScript can be used to store ____.",
+            options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+            answer: "all of the above",
+        },
+        {
+            question: "String values must be enclosed within _____ when being assinged to variables.",
+            options: ["commas", "curly brackets", "quotes", "parentheses"],
+            answer: "quotes",
+        },
+        {
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:____?",
+            options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+            answer: "console.log",
+        }
+    ];
 
-function showQuestion() {
-   const quizDisplayP = document.getElementById('askQuestion');
-   
-   questions.forEach((_question) => {
-    quizDisplayP.innerHTML = _question.question;
-    const options = _question.options;
-    options.forEach((option) => {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(option));
-    answersUl.appendChild(li);
-    })
-   })
-}
+    const answersUl = document.getElementById('answers');
 
-// for loop that cycles through questions
-for (var i = 0; i < questions.length; i++) {
-    questions[i] //inner.HTML updates as we go through the array.
-}
+    function showQuestion() {
+        const quizDisplayP = document.getElementById('askQuestion');
+        quizDisplayP.innerHTML = questions[currentIndx].question
+
+        // for loop that cycles through questions
+        for (var i = 0; i < questions[currentIndx].options.length; i++) {
+            const options = questions[currentIndx].options[i]
+            const li = document.createElement("li");
+            li.setAttribute("class", "options");
+            li.appendChild(document.createTextNode(options));
+            answersUl.appendChild(li);
+            var elements = document.getElementsByClassName("options");
+            elements[i].addEventListener('click', function () {
+                answersUl.innerHTML = "";
 
 
-//Scoreboard and initals
-const scores = [];
-const initals = document.getElementById('initials');
-const initialsList = document.getElementById('initialsList');
+                // grab the li that was clicked and compare it to the current question's answer 
+                var selecedOption = this.innerText;
+                var correctOption = questions[currentIndx].answer
+                console.log(selecedOption, correctOption)
+                // conditional statement, if correct do somethitng else do somethting 
+                if (selecedOption === correctOption) {
+                    alert("correct")
+                    score = score + 1;
+                    localStorage.setItem("totalScore", JSON.stringify(score))
+                } else {
+                    secondsLeft = secondsLeft - 10;
+                    alert("incorrect")
+                }
+                currentIndx++
+                showQuestion();
+            });
+        }
+    }
 
-function addInitals () {
-  const userInput = initals.value;
-   if (userInput == "") {
-      return
-  }
-   else {
-    scores.push(userInput);
-    initialsList.innerHTML = "";
-    scores.forEach((_score) => {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(_score));
-        initialsList.appendChild(li);
-    })
-  }
-}
-var submitButton = document.querySelector("#submit");
-submitButton.addEventListener('click', addInitals);
+    //Scoreboard and initals
+    const scores = [];
+    const initals = document.getElementById('initials');
+    const initialsList = document.getElementById('initialsList');
 
-//quiz (re)start button event
-startButton.addEventListener('click', setTime);
-showQuestion();
+    function addInitals() {
+        const userInput = initals.value;
+        if (userInput == "") {
+            return
+        }
+        else {
+            scores.push(userInput);
+            initialsList.innerHTML = "";
+            scores.forEach((_score) => {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(_score));
+                initialsList.appendChild(li);
+            })
+        }
+    }
+    var submitButton = document.querySelector("#submit");
+    submitButton.addEventListener('click', addInitals);
+
+    //quiz (re)start button event
+    startButton.addEventListener('click', setTime);
+    showQuestion();
 }
